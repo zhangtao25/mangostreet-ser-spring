caa3e399d72f1fd583bd7f98caa201dfd68ef759
zhangtao25 <zhangtao25@lenovo.com>
2020-01-26 22:49:19 +0800

    前期学这种东西好累

--- src/main/java/com/mangostreet/mangostreetserspring/controller/UserController.java
+++ src/main/java/com/mangostreet/mangostreetserspring/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.mangostreet.mangostreetserspring.controller;
 
+import com.mangostreet.mangostreetserspring.config.exception.AjaxResponse;
 import com.mangostreet.mangostreetserspring.dao.UserRepository;
 import com.mangostreet.mangostreetserspring.domain.UserDO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,5 +30,31 @@ public class UserController {
         user.setPassword(password);
         userRepository.save(user);
         return user;
+    }
+
+    @RequestMapping(value = "/auth/login", method = RequestMethod.GET)
+    @ResponseBody
+    public AjaxResponse authLogin(@RequestParam String username,
+                                  @RequestParam String password){
+
+        UserDO u =new UserDO();
+        u.setUserName(username);
+        u.setPassword(password);
+        Example<UserDO> example = Example.of(u);
+
+        UserDO user = userRepository.findOne(example).orElse(null);
+
+        if (user == null){
+
+            UserDO uu =new UserDO();
+            uu.setUserName(username);
+            uu.setPassword(password);
+            uu.setId(2);
+            userRepository.save(uu);
+            return AjaxResponse.success();
+        } else {
+            return AjaxResponse.success(user);
+        }
+
     }
 }
